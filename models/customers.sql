0c1dddf3809861a73af684096906304725a0bc36
lachlan pyne <lachlan.pyne@dbtlabsworkshop.com>
2026-02-19 06:00:37 +0000

    added model

--- models/customers.sql
+++ models/customers.sql
@@ -1,56 +1,54 @@
-with customers as (
+{{
+    config(
+        materialized='table'
+    )
+}}
 
-    select
-        id as customer_id,
-        first_name,
-        last_name
+with
+    customers as (
 
-    from raw.jaffle_shop.customers
+        select id as customer_id, first_name, last_name from raw.jaffle_shop.customers
 
-),
+    ),
 
-orders as (
+    orders as (
 
-    select
-        id as order_id,
-        user_id as customer_id,
-        order_date,
-        status
+        select id as order_id, user_id as customer_id, order_date, status
 
-    from raw.jaffle_shop.orders
+        from raw.jaffle_shop.orders
 
-),
+    ),
 
-customer_orders as (
+    customer_orders as (
 
-    select
-        customer_id,
+        select
+            customer_id,
 
-        min(order_date) as first_order_date,
-        max(order_date) as most_recent_order_date,
-        count(order_id) as number_of_orders
+            min(order_date) as first_order_date,
+            max(order_date) as most_recent_order_date,
+            count(order_id) as number_of_orders
 
-    from orders
+        from orders
 
-    group by 1
+        group by 1
 
-),
+    ),
 
+    final as (
 
-final as (
+        select
+            customers.customer_id,
+            customers.first_name,
+            customers.last_name,
+            customer_orders.first_order_date,
+            customer_orders.most_recent_order_date,
+            coalesce(customer_orders.number_of_orders, 0) as number_of_orders
 
-    select
-        customers.customer_id,
-        customers.first_name,
-        customers.last_name,
-        customer_orders.first_order_date,
-        customer_orders.most_recent_order_date,
-        coalesce(customer_orders.number_of_orders, 0) as number_of_orders
+        from customers
 
-    from customers
+        left join customer_orders using (customer_id)
 
-    left join customer_orders using (customer_id)
+    )
 
-)
-
-select * from final
+select *
+from final
